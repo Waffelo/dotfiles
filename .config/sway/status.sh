193f8eedc3e8371bf817aacda6ef36b73e2b9295
Waffelo <contact@waffelo.net>
2025-08-20 21:28:33 +0000

    ADDED: Battery indicator ADDED: CPU temperature FIXED: Sound volume indicator

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -2,35 +2,59 @@
 
 MULLVAD=$(
 if mullvad status | grep -q "Connected" ; then
-  #echo "🦊: $(mullvad status | cut -d " " -f5-)"
-  #echo "🦊: $(mullvad status | head -1 | cut -c31-60)"
-  echo "[🦊:🪢]"
+  echo "[🦊🪢]"
 else
   echo "[🦊💤]"
 fi
 )
 
 HDD=$(
-	Drives=("/dev/mapper/hdd1" "/dev/mapper/hdd2")
+Drives=("/dev/mapper/hdd1" "/dev/mapper/hdd2")
 
 for drive in "${Drives[@]}"; do
-	DName="[💾 $(echo $drive | awk -F '/' '{print $4}' | tr 'a-z' 'A-Z'):"
-	echo $DName
-	if [  -b "$drive" ]; then
-		echo "$(df -h | grep "$drive" | awk '{print $5}')]"
-	else
-		echo "🚫]"
-	fi
+    if [ -e "$drive" ]; then
+        DName="[💾 $(echo $drive | awk -F '/' '{print $4}' | tr 'a-z' 'A-Z'):"
+        echo $DName
+        if [  -b "$drive" ]; then
+            echo "$(df -h | grep "$drive" | awk '{print $5}')]"
+        else
+            echo "🚫]"
+        fi
+    fi
 done
 )
 
+BATTERY=$(
+if [ -e /sys/class/power_supply/BAT0/capacity ]; then
+    bat_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+    bat_status=$(cat /sys/class/power_supply/BAT0/status)
+
+    if [ "$bat_status" == "Discharging" ]; then
+        echo "[🔋📉 ${bat_capacity}%]"
+    elif [ "$bat_status" == "Charging" ]; then
+        echo "[🔋🔌 ${bat_capacity}%]"
+    else
+        echo "[🔋 ${bat_capacity}%]"
+    fi 
+fi
+)
+
+CPU_TEMP=$(
+    if [ -f /sys/class/thermal/thermal_zone0/temp ]; then
+        cpu_temp=$(cat /sys/class/thermal/thermal_zone0/temp)
+        cpu_temp=$((cpu_temp / 1000))
+
+        echo "[🔲 ${cpu_temp}°C]"
+    fi
+)
+
+
 LAYOUT="[ ⌨️ $(swaymsg -t get_inputs | grep "active_layout_name" | awk '{print $2 $3}' | sed s/\"//g | sed s/,//g | tail -n 1 | cut -c 1-3) ]"
-VOLUME="[🔊 $(pactl get-sink-volume alsa_output.pci-0000_0c_00.4.analog-stereo | awk '{print $5}')]"
+VOLUME="[🔊 $(pulsemixer --get-volume | awk '{print $1}')%]"
 MEMORY="[🐏 $(free -h | head -2 | tail -1 | awk '{print $3}')/$(free -h | head -2 | tail -1 | awk '{print $2}')]"
 DATE="[📅 $(date "+%d/%m/%y")]"
 TIME="[🕓 $(date "+%H:%M:%S")]"
 
 
-
 # --- Main
-echo $HDD $LAYOUT $MULLVAD $VOLUME $MEMORY $DATE $TIME
+echo $HDD $LAYOUT $MULLVAD $VOLUME $MEMORY $CPU_TEMP $BATTERY $DATE $TIME
